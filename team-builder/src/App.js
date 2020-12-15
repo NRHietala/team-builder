d6ab096e9239ac3decb2d6229d50f3963d7ec1f7
Nathan Hietala <Nrhietala@gmail.com>
2020-12-15 14:03:05 -0600

    Working on helper functions and validation

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,27 +1,70 @@
 import './App.css';
 import React, { useState } from 'react';
 import Form from './components/Form'
-function App() {
 
-  const initialFormValues = {
-    name:"",
-    email:"",
-    role:"",
+
+const initialFormValues = {
+  name:"",
+  email:"",
+  role:"",
+}
+
+const dummyFriends = [
+  {
+    id:123,
+    name:'Nate',
+    email: 'Nate@nate.com',
+    role: 'Fullstack'
+  },
+  {
+    id:456,
+    name:'Joe',
+    email: 'Joe@Joe.com',
+    role: 'Back-End'
+  },
+  {
+    id:789,
+    name:'Ana',
+    email: 'Ana@Ana.com',
+    role: 'Front-end'
+  },
+];
+
+function App() {
+  const [ formValues, setFormValues ] = useState(initialFormValues);
+  const [ friends, setFriends ] = useState(dummyFriends)
+  // helper functions for submit and onchange
+
+  const onChange = event => {
+    const { name, value } = event.target;
+
   }
 
-  const [ formValues, setFormValues ] = useState(initialFormValues);
+  function onSubmit (inputName, inputValue) {
 
-  // helper functions for submit and onchange
+  }
 
   
 
   return (
     <div className="App">
-      App working
+      Simple Team Building Form!
       <Form
       values={formValues}
-      // add helpers
+      onChange={onChange}
       />
+      {
+        friends.map(friend => {
+          return (
+            <div key={friend.id}>
+              <h2>{friend.name}</h2>
+              <h3>{friend.email}</h3>
+              <h3>{friend.role}</h3>
+              <hr/>
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Form(props) {
-  const { name, email, role } = props;
+  const { values, onChange, onSubmit } = props;
 
   return (
     <div>
@@ -10,22 +10,22 @@ function Form(props) {
           <input 
           type="text"
           name="name"
-          // onChange={onChange}
-          value={name}
+          onChange={onChange}
+          value={values.name}
           />
         </label>
         <label>Email
           <input 
           type="email"
           name="email"
-          // onChange={onChange}
-          value={email}
+          onChange={onChange}
+          value={values.email}
           />
         </label>
         <label>
           <select name="role">
             <option value="">---Select One---</option>
-            <option value="Front-End">Front-End</option>
+            <option value="Front-end">Front-End</option>
             <option value="Back-end">Back-End</option>
             <option value="Fullstack">Fullstack</option>
           </select>
